9f72cea5bc65764a9d3259bc368e49321811140a
perle alkhodari <perlealkhodari@perles-MacBook-Pro.local>
2025-01-12 18:30:56 +0200

    handled the login with user feedback and stuff

--- index.js
+++ index.js
@@ -44,21 +44,29 @@ app.get('/secrets', (req, res)=> {
 })
 
 app.post('/register', async (req, res)=> {
+    // Getting form data
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
 
+    // Putting everything in a try block
     try {
+
         // Making sure the email is unique...
         var result = await db.query(
             "SELECT * FROM users WHERE email = $1",
             [email]
         );
+
         if (result.rows.length > 0) {
+
             // Handle the error
             res.render("register.ejs", {error: "An account is already registered with this email. Try logging in."})
+
         }
+
         else {
+            
             // Otherwise add the new user into the database
             await db.query(
                 "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
@@ -67,15 +75,58 @@ app.post('/register', async (req, res)=> {
 
             res.render("secrets.ejs");
         }
-    } catch(err) {
+    } 
+
+    catch(err) {
         console.log(err);
     }
 
 })
 
-app.post('/login', (req, res)=> {
+app.post('/login', async (req, res)=> {
+    // Getting form data
     const email = req.body.email;
     const password = req.body.password;
+
+    // Putting everything in a try catch for emergency error handling
+    try {
+        
+        // Making sure this user exists in the first place...
+        const result = await db.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (result.rows.length == 0) {
+
+            // Let user know that this email is not registered
+            res.render("login.ejs", {emailError: "This email is not registered."})
+
+        }
+
+        else {
+
+            // Since the user exists, check if the password is correct
+            var user = result.rows;
+            var savedPassword = user[0].password;
+
+            // If the passwords match
+            if (savedPassword == password) {
+                // let the user see the secrets
+                res.render("secrets.ejs");
+            }
+
+            // Otherwise take them back to login with feedback
+            else {
+                res.render("login.ejs", {passwordError: "The password is incorrect."});
+            }
+        }
+
+    }
+
+    catch (err) {
+        console.log(err);
+    }
     
 })
 
