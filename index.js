e8123bfbb11c59a53edcc5246c90094f541f0d51
perle alkhodari <perlealkhodari@perles-MacBook-Pro.local>
2025-01-12 16:27:35 +0200

    saving progress on this website.

---
+++ index.js
@@ -0,0 +1,33 @@
+// Importing packages I need
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import {fileURLToPath} from "url"
+
+// Setting some essential constants
+const __dirname = dirname(fileURLToPath(import.meta.url));  // Getting the exact full path to this folder
+const app = express();                                  // Initializing app
+const port = 3000;                                      // Setting application port number
+
+// Middlware
+app.use(bodyParser.urlencoded({extended: true}));       // Setting up the body parser for forms
+app.use(express.static(__dirname + "/public"));         // Showing my express application where the public folder is
+
+// Home page get route
+app.get('/', (req, res)=> {
+    res.render("index.ejs");
+})
+
+app.get('/login', (req, res)=> {
+    res.render("login.ejs");
+})
+
+app.get('/register', (req, res)=> {
+    res.render("register.ejs");
+})
+
+// App listener
+app.listen(port, ()=> {
+    console.log(`Listening to port ${port}.`);
+})
