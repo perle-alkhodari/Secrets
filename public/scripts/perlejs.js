ff72c03b1f81d828d8ff7f8692c92a1820cf1344
perle alkhodari <perlealkhodari@perles-MacBook-Pro.local>
2025-01-15 00:01:08 +0200

    added animation to homepage

---
+++ public/scripts/perlejs.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    var exploreMessage = $(".exlore-message");
+
+    var homeUsername = $(".home-username");
+    homeUsername.animate({marginTop: '0px', opacity: 1 }, 1000, function(){
+        exploreMessage.animate({ marginTop: '0px', opacity: 1}, 1000,);
+    }
+    );
+
+});
+
+
